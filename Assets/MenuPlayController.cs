00e6ce566a71d7f9c4f2f81c2adb1ff29d7a1836
Jose Ignacio Huby Ochoa <jose.huby@utec.edu.pe>
2023-06-24 16:21:14 -0500

    menu integrado

--- Assets/MenuPlayController.cs
+++ Assets/MenuPlayController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MenuPlayController : MonoBehaviour
 {
@@ -34,7 +35,7 @@ public class MenuPlayController : MonoBehaviour
     }
 
     public void PlaySong(){
-
+        SceneManager.LoadScene(1);
     }
 
     private void ChangeSong(){

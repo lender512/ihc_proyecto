ec3d251cbcb415b732e745de92ee87b626d3f824
Jose Ignacio Huby Ochoa <jose.huby@utec.edu.pe>
2023-06-24 16:13:53 -0500

    Cambios en el menu

--- Assets/MenuPlayController.cs
+++ Assets/MenuPlayController.cs
@@ -1,14 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuPlayController : MonoBehaviour
 {
-    public void PrevSong(){
-        
+    private int selectedSongId = 0;
+
+    public GameObject banner;
+
+    public List<Sprite> sprites;
+
+    private int size = 0;
+
+    void Start(){
+        ChangeSong();
+        Debug.Log("HOLAA");
+        size = sprites.Count;
     }
-        public void NextSong(){
-        
+
+    public void PrevSong(){
+        bool first = selectedSongId == 0;
+        if(first){
+            selectedSongId = size-1;
+        }else{
+            selectedSongId--;
+        }
+        ChangeSong();
+    }
+    public void NextSong(){
+        selectedSongId = (selectedSongId+1)%size;
+        ChangeSong();
+    }
+
+    public void PlaySong(){
+
+    }
+
+    private void ChangeSong(){
+         banner.GetComponent<Image>().sprite = sprites[selectedSongId];
     }
 
     
